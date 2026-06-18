f818c3e85ae43c9b76762603e26557b12662894a
Alexander Dao <alvdao@ucsc.edu>
2026-06-18 12:51:34 -0700

    latest

--- spotify_api.py
+++ spotify_api.py
@@ -3,7 +3,7 @@ from spotipy.oauth2 import SpotifyOAuth
 import sqlite3
 from datetime import datetime
 
-def spotify_api_handler():
+def spotify_api_handler(): # this 
     sp = spotipy.Spotify(
         auth_manager = SpotifyOAuth(
             client_id='YOUR_CLIENT_ID',
@@ -11,3 +11,14 @@ def spotify_api_handler():
             redirect_uri='YOUR_REDIRECT_URI',
             scope='user-library-read')
             )
+
+    if sp is not None:
+        print("Connection was established.")
+        return True
+    # else:
+    print("Connection failed.")
+    return False
+    # we know this part works
+
+if __name__ == "__main__":
+    spotify_api_handler()
--- spotify_apitest.py
+++ spotify_apitest.py
@@ -1,22 +1,24 @@
+import os
+from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-import sqlite3
-from datetime import datetime
+# import json
+load_dotenv() # pass by reference for my env file 
 
-# TEST CASES FOR API CONNECTION FOR PULLING RAW DATA
+# API CONNECTION TEST CASES.
 # first test: establish basic connection to spotify API
-
 def test_spotify_connection():
-    # spotipy will do the heavy lifting
+    # spotipy will do the heavy lifting for oauth
     sp = spotipy.Spotify(
         auth_manager = SpotifyOAuth(
-            client_id='YOUR_CLIENT_ID',
-            client_secret='YOUR_CLIENT_SECRET',
-            redirect_uri='YOUR_REDIRECT_URI',
-            scope='user-library-read')
+            client_id = os.getenv('SPOTIFY_CLIENT_ID'),
+            client_secret = os.getenv('SPOTIFY_CLIENT_SECRET'),
+            # i'm guessing this is where our api creds go
+            redirect_uri = os.getenv('SPOTIFY_REDIRECT_URI'), 
+            # i'm guessing this is for the uri to redirect to after you're done?
+            scope='user-library-read') # look at user's tracks
             )
     
-    # this gets the current user's saved tracks?
     # we're testing the connection to the API to start
     if sp is not None:
         print("Connection was established.")
@@ -24,6 +26,9 @@ def test_spotify_connection():
     # else:
     print("Connection failed.")
     return False
+
+def test_user_data_request():
+    artist_uri = None # hasn't been assigned yet
     
-if __name__ == "__main__":
-    test_spotify_connection()
+    if __name__ == "__main__":
+        test_spotify_connection()
